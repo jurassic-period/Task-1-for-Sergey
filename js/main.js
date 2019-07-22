76f358f9a8b74ea20fa1777e2dd266160b31f827
Yuriy Sydarskiy <ya.donny@gmail.com>
2019-07-22 20:22:34 +0300

    second commit a lot of changes everythink work with 1 word

--- js/main.js
+++ js/main.js
@@ -1,14 +1,40 @@
 const result = () => {
-    const strFromInput = document.getElementById("inputTxt").value;
-    alert(isPalindrome(strFromInput));
+    const str = document.getElementById("inputTxt").value;
+    if (str[0] === ' ') {
+
+      if (str.length === 1) {
+        return alert(null);
+      } 
+
+      else if (str[str.length - 1] === ' ') {
+      const cutSpace = str.substring(1, str.length - 1);
+      alert(isPalindrome(cutSpace));
+      } 
+
+      else {
+      const cutSpace = str.substring(1);
+      alert(isPalindrome(cutSpace));
+      }
+
+    } 
+      else if (str[str.length - 1] === ' ') {
+      const cutSpace = str.substring(0, str.length - 1);
+      alert(isPalindrome(cutSpace));
+      } 
+      else {
+      alert(isPalindrome(str));
+      }   
 };
 
 const isPalindrome = (str) => {
     if (str.length === 1) {
       return true;
-    } else if (str[0] === str[str.length - 1]) {
-      const strWithoutOneAndLastChar = str.substring(1, str.length - 1);
-      return isPalindrome(strWithoutOneAndLastChar);
     } 
+
+    else if (str[0] === str[str.length - 1]) {
+    const strWithoutOneAndLastChar = str.substring(1, str.length - 1);
+    return isPalindrome(strWithoutOneAndLastChar);
+    } 
+    
     return false;
   };
