7983bca36c8762aaff35dfe62d3dfe12d5d05afe
Yuriy Sydarskiy <ya.donny@gmail.com>
2019-07-23 12:50:42 +0300

    The seventh commit added the removal of spaces

--- js/main.js
+++ js/main.js
@@ -1,26 +1,10 @@
 const result = () => {
   const str = document.getElementById("inputTxt").value;
-  if (str[0] === " ") {
-    // Checking for empty space infront string
-
-    if (str.length === 1) {
-      // If it is just space
-      return alert(null);
-    } else if (str[str.length - 1] === " ") {
-      // Checking for empty space infront and after string
-      const cutSpace = str.substring(1, str.length - 1);
-      alert(isPalindrome(cutSpace));
-    } else {
-      const cutSpace = str.substring(1); // if we have only 1 space infront string
-      alert(isPalindrome(cutSpace));
-    }
-  } else if (str[str.length - 1] === " ") {
-    // if we have only 1 space after string
-    const cutSpace = str.substring(0, str.length - 1);
-    alert(isPalindrome(cutSpace));
+  if (str[0] === " " && str.length === 1) {
+    return alert(null);
   } else {
-    alert(isPalindrome(str));
-  }
+  alert(isPalindrome(str.trim())); // remove spaces
+}
 };
 
 const isPalindrome = str => {
