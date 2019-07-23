21e424096a8c9278bf0375df39215e665ca21193
Yuriy Sydarskiy <ya.donny@gmail.com>
2019-07-23 16:10:14 +0300

    The thirteenth add constraction Try

--- js/main.js
+++ js/main.js
@@ -5,38 +5,42 @@ const answer = (str) => {
 
 const result = () => {
   const str = document.getElementById("inputTxt").value.trim();
-  if (str === '') {
-    return answer('You have sent an empty string');
-  } else {
-    return answer(isPalindromeSecond(str)); // remove spaces
+  try {
+    if (str === '') {
+      return answer('You have sent an empty string');
+    } else {
+      return answer(isPalindrome(str)); // remove spaces
+    }
+  } catch {
+    document.getElementById('Output').innerHTML = 'Sorry an error has occurred';
   }
 };
 
-// const isPalindrome = str => {
-//   if (str.length === 1) {
-//     // base case to stop recursion
-//     return true;
-//   } else if (str[0] === str[str.length - 1]) {
-//     if (str.length === 2) {
-//       return true;
-//     }
-//     const strWithoutOneAndLastChar = str.substring(1, str.length - 1); // move deep into the working condition
-//     return isPalindrome(strWithoutOneAndLastChar);
-//   }
-
-//   return false;
-// };
-
-
-const isPalindromeSecond = (str) => {
-  let now = str;
-  for (;;) {
-    if (now[0] === now[str.length - 1]) {
-      now = str.substring(1, str.length - 1);
-    } if (now.length <= 1) {
+const isPalindrome = str => {
+  if (str.length === 1) {
+    // base case to stop recursion
+    return true;
+  } else if (str[0] === str[str.length - 1]) {
+    if (str.length === 2) {
       return true;
-    } return false;
-  } 
+    }
+    const strWithoutOneAndLastChar = str.substring(1, str.length - 1); // move deep into the working condition
+    return isPalindrome(strWithoutOneAndLastChar);
+  }
+
+  return false;
 };
+
+
+// const isPalindromeSecond = (str) => { \\ backup option
+//   let now = str;
+//   for (;;) {
+//     if (now[0] === now[str.length - 1]) {
+//       now = str.substring(1, str.length - 1);
+//     } if (now.length <= 1) {
+//       return true;
+//     } return false;
+//   } 
+// };
 
 btn.onclick = result;
