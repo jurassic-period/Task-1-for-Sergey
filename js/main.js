c3e8c011ec485d067b9815515e62e9f1de59767c
Yuriy Sydarskiy <ya.donny@gmail.com>
2019-07-23 13:33:50 +0300

    The tenth a lot of changes - new function for Palindrome

--- js/main.js
+++ js/main.js
@@ -3,23 +3,36 @@ const result = () => {
   if (str === '') {
     return alert(null);
   } else {
-    alert(isPalindrome(str)); // remove spaces
+    alert(isPalindromeSecond(str)); // remove spaces
   }
 };
 
-const isPalindrome = str => {
-  if (str.length === 1) {
-    // base case to stop recursion
-    return true;
-  } else if (str[0] === str[str.length - 1]) {
-    if (str.length === 2) {
-      return true;
-    }
-    const strWithoutOneAndLastChar = str.substring(1, str.length - 1); // move deep into the working condition
-    return isPalindrome(strWithoutOneAndLastChar);
-  }
+// const isPalindrome = str => {
+//   if (str.length === 1) {
+//     // base case to stop recursion
+//     return true;
+//   } else if (str[0] === str[str.length - 1]) {
+//     if (str.length === 2) {
+//       return true;
+//     }
+//     const strWithoutOneAndLastChar = str.substring(1, str.length - 1); // move deep into the working condition
+//     return isPalindrome(strWithoutOneAndLastChar);
+//   }
 
-  return false;
+//   return false;
+// };
+
+
+const isPalindromeSecond = (str) => {
+  let now = str;
+  for (;;) {
+    if (now[0] === now[str.length - 1]) {
+      now = str.substring(1, str.length - 1)
+      console.log(now);
+    } if (now.length <= 1) {
+      return true;
+    } return false;
+  } 
 };
 
 btn.onclick = result;
