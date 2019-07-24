6bdd05471087c209c5da8e737e40f092d038a7f5
Yuriy Sydarskiy <ya.donny@gmail.com>
2019-07-24 16:42:46 +0300

    17 sort array + while

--- js/main.js
+++ js/main.js
@@ -15,7 +15,7 @@ const result = () => {
     answer('Sorry an error has occurred');
   }
 };
-
+/*_____________________________Palindrome_with_recursion__________________________ */
 const isPalindrome = str => {
   if (str.length === 1) {
     // base case to stop recursion
@@ -30,7 +30,7 @@ const isPalindrome = str => {
 
   return false;
 };
-
+/*_______________________________Palindrome_with_cycle____________________________ */
 
 // const isPalindromeSecond = (str) => { \\ backup option
 //   let now = str;
@@ -54,7 +54,7 @@ const answer2 = (str) => {
 
 const result2 = () => {
   try {
-    const arr = document.getElementById("inputArray").value.trim();
+    const arr = document.getElementById("inputArray").value.trim().split('');
     if (arr === '') {
       return answer2('You have sent an empty string');
     } else {
@@ -65,10 +65,34 @@ const result2 = () => {
   }
 };
 
+/*____________________________________Cycle-for___________________________________ */
+
+
+// const sortArr = (arr) => {
+//   let unit;
+//   for (let i = arr.length - 1; i > 0; i--) {
+//     for (let b = 0; b < i; b++) {
+//       if (arr[b] > arr[b + 1]) {
+//         unit = arr[b];
+//         arr[b] = arr[b + 1];
+//         arr[b + 1] = unit;
+//       }
+//     }
+//   } 
+//   return arr;
+// };
+
+/*____________________________________Cycle-whille___________________________________ */
+
+
 const sortArr = (arr) => {
   let unit;
-  for (let i = arr.length - 1; i > 0; i--) {
-    for (let b = 0; b < i; b++) {
+  let i = arr.length;
+  while (i > 0) {
+    i -= 1;
+    let b = 0;
+    while (b < i) {
+      b += 1;
       if (arr[b] > arr[b + 1]) {
         unit = arr[b];
         arr[b] = arr[b + 1];
