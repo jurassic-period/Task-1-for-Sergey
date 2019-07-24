f9262048fa82895449e55299149bb3b030dd17be
Yuriy Sydarskiy <ya.donny@gmail.com>
2019-07-24 15:54:36 +0300

    16 + array sort form

--- js/main.js
+++ js/main.js
@@ -44,3 +44,39 @@ const isPalindrome = str => {
 // };
 
 btn.onclick = result;
+
+/*____________________________________Array-Form___________________________________ */
+
+const answer2 = (str) => {
+  const out = document.getElementById('Output2').innerHTML = str;
+  return out;
+};
+
+const result2 = () => {
+  try {
+    const arr = document.getElementById("inputArray").value.trim();
+    if (arr === '') {
+      return answer2('You have sent an empty string');
+    } else {
+      return answer2(sortArr(arr)); 
+    }
+  } catch {
+    answer2('Sorry an error has occurred');
+  }
+};
+
+const sortArr = (arr) => {
+  let unit;
+  for (let i = arr.length - 1; i > 0; i--) {
+    for (let b = 0; b < i; b++) {
+      if (arr[b] > arr[b + 1]) {
+        unit = arr[b];
+        arr[b] = arr[b + 1];
+        arr[b + 1] = unit;
+      }
+    }
+  } 
+  return arr;
+};
+
+btn2.onclick = result2;
